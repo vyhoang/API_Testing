634f00bdbfff7049639ed136e34260c4ec70d52a
vyhoang <vyht444@gmail.com>
2021-06-17 14:46:31 -0500

    Revert "update params in GetBook request"
    This reverts commit 34a8ded6

--- apiValidation.py
+++ apiValidation.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 response = requests.get('http://216.10.245.166/Library/GetBook.php',
-                        params={'AuthorName': 'Vy Hoang'}, )
+                        params={'AuthorName': 'Rahul Shetty2'}, )
 # print(reponse.text)
 # print(type(reponse.text))
 #
